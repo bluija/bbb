f739bdc4ff5ade94dc3fe9c160a3266dca5df5d5
ZhaoBin Li <173764296+bluija@users.noreply.github.com>
2025-04-24 10:00:02 -0700

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9YugPgSfGUAoCIK18H3Mgk_jyyXbLNoM",
+  authDomain: "mreu-b818d.firebaseapp.com",
+  databaseURL: "https://mreu-b818d-default-rtdb.firebaseio.com",
+  projectId: "mreu-b818d",
+  storageBucket: "mreu-b818d.firebasestorage.app",
+  messagingSenderId: "719952825793",
+  appId: "1:719952825793:web:8a69cb0c3d708dfa987340"
+};
